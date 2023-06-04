9bbaab91dc96f5ca1b846d3b72375b4989ca84bf
Huong Le <huong10102001@gmail.com>
2023-06-04 18:06:36 +0700

    fix bug for feature comment and ratting

--- src/frontend/src/views/post/detail/ContentSection.vue
+++ src/frontend/src/views/post/detail/ContentSection.vue
@@ -3,7 +3,7 @@
 		<h2 class="title is-3">{{ content.title }}</h2>
 
 		<div v-for="line in content.paragraph">
-			<p class="my-4">{{ line.text }}</p>
+			<p class="my-5">{{ line.text }}</p>
 			<div v-if="line.below_img">
 				<figure class="image is-2by1 my-6">
 					<img :src=content.image>
