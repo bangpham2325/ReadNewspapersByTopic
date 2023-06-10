234f5fb60104a5994d4914abf8e15497b67d709b
BangPham <bang.pham@paradox.ai>
2023-06-10 17:08:08 +0700

    task add filter by category

--- src/backend/api_post/services/post.py
+++ src/backend/api_post/services/post.py
@@ -45,6 +45,9 @@ class PostService(BaseService):
     @classmethod
     def get_list_post_by_category(cls, params=None):
         ft = Q(status=PostStatus.PUBLISHED.value)
+        if params.getlist('categories'):
+            topic_ids = params.getlist('categories')
+            ft &= Q(category__id__in=topic_ids)
         if params.get('search'):
             search_string = params.get('search')
             ft &= (Q(author__contains=search_string) | Q(category__title__contains=search_string) | Q(
@@ -54,14 +57,14 @@ class PostService(BaseService):
         posts = Posts.objects.annotate(title_lower=Lower('title')).filter(ft).prefetch_related(
             'category').prefetch_related('source').prefetch_related('post_rating').annotate(
             avg_rating=Avg("post_rating__star_rating"))
-        if params.getlist('categories'):
-            topic_ids = params.getlist('categories')
-            posts = posts.filter(category__id__in=topic_ids)
         return posts
 
     @classmethod
-    def get_list_post_by_favourite(cls):
+    def get_list_post_by_favourite(cls, params=None):
         ft = Q(status=PostStatus.PUBLISHED.value)
+        if params.getlist('categories'):
+            topic_ids = params.getlist('categories')
+            ft &= Q(category__id__in=topic_ids)
         posts = Posts.objects.prefetch_related('post_rating').prefetch_related('category').prefetch_related(
             'source').annotate(avg_rating=Avg('post_rating__star_rating')).filter(ft).order_by('-avg_rating')[:8]
         return posts
@@ -69,6 +72,9 @@ class PostService(BaseService):
     @classmethod
     def get_list_post_by_views(cls, params=None):
         ft = Q(status=PostStatus.PUBLISHED.value)
+        if params.getlist('categories'):
+            topic_ids = params.getlist('categories')
+            ft &= Q(category__id__in=topic_ids)
         posts = Posts.objects.filter(ft).prefetch_related('post_rating').prefetch_related('category').prefetch_related(
             'source').annotate(avg_rating=Avg('post_rating__star_rating')).order_by('-views')[:8]
         return posts
@@ -76,6 +82,9 @@ class PostService(BaseService):
     @classmethod
     def get_list_post_by_likes(cls, params=None):
         ft = Q(status=PostStatus.PUBLISHED.value)
+        if params.getlist('categories'):
+            topic_ids = params.getlist('categories')
+            ft &= Q(category__id__in=topic_ids)
         posts = Posts.objects.prefetch_related('post_rating').prefetch_related('category').prefetch_related(
             'source').annotate(avg_rating=Avg('post_rating__star_rating')).filter(ft).order_by('-likes')[:8]
         return posts
@@ -83,6 +92,9 @@ class PostService(BaseService):
     @classmethod
     def get_list_new_post(cls, params=None):
         ft = Q(status=PostStatus.PUBLISHED.value)
+        if params.getlist('categories'):
+            topic_ids = params.getlist('categories')
+            ft &= Q(category__id__in=topic_ids)
         posts = Posts.objects.prefetch_related('post_rating').prefetch_related('category').prefetch_related(
             'source').annotate(avg_rating=Avg('post_rating__star_rating')).filter(ft).order_by('-publish_date')[:50]
         return posts
@@ -90,6 +102,9 @@ class PostService(BaseService):
     @classmethod
     def get_post_management(cls, params=None):
         ft = Q()
+        if params.getlist('categories'):
+            topic_ids = params.getlist('categories')
+            ft &= Q(category__id__in=topic_ids)
         if params.get('search'):
             search_string = params.get('search')
             ft &= (Q(author__contains=search_string) | Q(category__title__contains=search_string) | Q(
@@ -102,9 +117,6 @@ class PostService(BaseService):
         posts = Posts.objects.annotate(title_lower=Lower('title')).filter(ft).prefetch_related(
             'category').prefetch_related('source').prefetch_related('post_rating'
                                                                     ).annotate(avg_rating=Avg("post_rating__star_rating"))
-        if params.getlist('categories'):
-            topic_ids = params.getlist('categories')
-            posts = posts.filter(category__id__in=topic_ids)
         return posts
 
     @classmethod
